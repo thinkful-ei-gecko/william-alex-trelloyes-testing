c756823fd06eb8ae0e73cd5600cd62aa77695cca
Alexander Fukui <alexander.fukui@gmail.com>
2019-09-04 12:54:42 -0500

    last commit, tests running well

--- src/Card.test.js
+++ src/Card.test.js
@@ -2,7 +2,6 @@ import React from 'react';
 import ReactDOM from 'react-dom';
 import renderer from 'react-test-renderer';
 import Card from './Card';
-import { exportAllDeclaration } from '@babel/types';
 
 
 describe('Card component', () => {
--- src/List.test.js
+++ src/List.test.js
@@ -5,21 +5,23 @@ import List from './List';
 
 
 describe('List Component Tests', ()=> {
-  // const testCards = {
-  //   'a': { id: 'a', title: 'First card', content: 'lorem ipsum' },
-  //   'b': { id: 'b', title: 'Second card', content: 'lorem ipsum' }
-  // }
   //Smoke Test
   it('List renders correctly', () => {
     const div = document.createElement('div');
-    ReactDOM.render(<List  />, div);
+    ReactDOM.render(<List header='string' cards={[
+      { id: 'a', title: 'First card', content: 'lorem ipsum' },
+      { id: 'b', title: 'Second card', content: 'lorem ipsum' },
+    ]} />, div);
     ReactDOM.unmountComponentAtNode(div);
   });
 
   //Snapshot
   it('List matches snapshot', () => {
     const tree = renderer
-      .create(<List />)
+      .create(<List header='string' cards={[
+        { id: 'a', title: 'First card', content: 'lorem ipsum' },
+        { id: 'b', title: 'Second card', content: 'lorem ipsum' },
+      ]} />)
       .toJSON();
       expect(tree).toMatchSnapshot();
   });
